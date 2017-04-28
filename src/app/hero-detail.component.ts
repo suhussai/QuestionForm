0da0e2087501b8e10f61072a266df8adcb146819
suhussai <suhussai@ualberta.ca>
2017-04-27 23:25:46 -0600

    remove hero refs

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -4,14 +4,6 @@ import { Hero }    from './hero';
 @Component({
 	selector: 'hero-detail',
 	template: `
-		<div *ngIf="hero">
-			<h2>{{hero.name}} details!</h2>
-			<div><label>id: </label>{{hero.id}}</div>
-			<div>
-				<label>name: </label>
-				<input [(ngModel)]="hero.name" placeholder="name"/>
-			</div>
-		</div>
 	`
 })
 
