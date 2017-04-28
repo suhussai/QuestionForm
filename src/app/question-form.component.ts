7be06cee46c6aa01b21ac0d4c75536f54b082f00
suhussai <suhussai@ualberta.ca>
2017-04-27 18:42:24 -0600

    add all questions

--- src/app/question-form.component.ts
+++ src/app/question-form.component.ts
@@ -49,19 +49,55 @@ export class QuestionFormComponent {
       "Yes",
       "No",
     ]),
-    new Question(3, "H3ow old are you?", [
-      "1",
-      "2",
-      "3",
-      "4",
+    new Question(3, "Do you suffer from dementia or cognitive impairment?", [
+      "Yes",
+      "No",
     ]),
+    new Question(4, "How would you describe your health this week?", [
+      "I am able to care for most of my own needs but I might need occasional assistance.",
+      "I need quite a bit of assistance and I might need frequent medical care.",
+      "I spend much of my time in bed or lying down.",
+    ]),
+    new Question(5, "Are you suffering from other chronic diseases or conditions?", [
+      "Few other health problems.",
+      "Heart disease.",
+      "Many other health problems (with or without heart disease).",
+    ]),
+    new Question(6, "How important is it for you to avoid being hospitalized?", [
+      "Very important.",
+      "Somewhat important.",
+      "Unsure",
+      "Somewhat unimportant",
+      "Unimportant",
+    ]),
+    new Question(7, "How important is it for you to avoid having major or minor surgery?", [
+      "Very important.",
+      "Somewhat important.",
+      "Unsure",
+      "Somewhat unimportant",
+      "Unimportant",
+    ]),
+    new Question(8, "How important is it for you to avoid traveling for dialysis or clinic visits?", [
+      "Very important.",
+      "Somewhat important.",
+      "Unsure",
+      "Somewhat unimportant",
+      "Unimportant",
+    ]),
+    new Question(9, "How important is it for you to have control over what you do with your time?", [
+      "Very important.",
+      "Somewhat important.",
+      "Unsure",
+      "Somewhat unimportant",
+      "Unimportant",
+    ])
 ];
 
   accordions = [
     new AccordionContent(2, "More Information", "Picture"),
     new AccordionContent(3, "More Information", "When people suffer from dementia or cognitive impairment, dialysis often makes them more confused. Dialysis is almost never a good choice for people with dementia or significant cognitive impairment."),
     new AccordionContent(4, "More Information", "The people who do best with dialysis are people who are functionally independent. If you spend much of your time in bed, or if you find you need help with day-to-day chores like dressing and bathing, dialysis probably will not help you much, if at all."),
-    new AccordionContent(7, "What is surgery?", "Picture"),
+    new AccordionContent(7, "What is surgery?", "Surgery is a procedure to diagnose or treat a medical problem.  It involves the use of instruments and cutting of body tissues."),
   ];
 page = 1;
 questionProgress = 0;
